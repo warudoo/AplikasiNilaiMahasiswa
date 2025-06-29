d870e59cc52dea5937ca3fbb3b89b6b4ea3957a0
warudo <muhammadsalwarud@gmail.com>
2025-06-29 23:47:59 +0700

    feat: for the filter user

--- src/java/com/nilai/controller/LoginFilter.java
+++ src/java/com/nilai/controller/LoginFilter.java
@@ -1,14 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.nilai.controller;
 
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 /**
- *
- * @author Warudo
+ * Filter ini akan mencegat semua permintaan ke halaman yang dilindungi (dalam hal ini "/nilai")
+ * dan memeriksa apakah pengguna sudah login atau belum.
  */
-public class LoginFilter {
+@WebFilter(filterName = "LoginFilter", urlPatterns = {"/nilai"})
+public class LoginFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        HttpSession session = req.getSession(false); // Ambil session yang ada, jangan buat baru
+
+        // Jika tidak ada session ATAU atribut "user" tidak ada di session, paksa ke halaman login
+        if (session == null || session.getAttribute("user") == null) {
+            res.sendRedirect(req.getContextPath() + "/login");
+        } else {
+            chain.doFilter(request, response); // Jika sudah login, izinkan akses ke tujuan
+        }
+    }
     
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
+    @Override
+    public void destroy() {}
 }
