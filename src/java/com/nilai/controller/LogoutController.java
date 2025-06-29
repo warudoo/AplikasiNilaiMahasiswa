83d6d35848be2388f522bcb76145dff345da1e2b
warudo <muhammadsalwarud@gmail.com>
2025-06-29 23:48:45 +0700

    feat: logic for logout

--- src/java/com/nilai/controller/LogoutController.java
+++ src/java/com/nilai/controller/LogoutController.java
@@ -9,6 +9,18 @@ package com.nilai.controller;
  *
  * @author Warudo
  */
-public class LogoutController {
-    
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "LogoutController", urlPatterns = {"/logout"})
+public class LogoutController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getSession().invalidate(); // Hapus semua data dari session
+        response.sendRedirect("login"); // Arahkan kembali ke halaman login
+    }
 }
